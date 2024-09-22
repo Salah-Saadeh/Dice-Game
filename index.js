8b602f71d2ec15adaa722c20c9f3ddb24f6bbc88
Salah-Saadeh <119014088+Salah-Saadeh@users.noreply.github.com>
2024-09-22 03:50:17 +0300

    Update index.js
    Added a shuffle button instead of refreshing the page to shuffle the dices

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var randomNum1 = Math.ceil((Math.random() * 6));
-document.getElementsByClassName("img1")[0].setAttribute("src" , "./images/dice"+randomNum1+".png");
+document.querySelector(".shuffleButton").addEventListener("click", function(){
+    var randomNum1 = Math.ceil((Math.random() * 6));
+    document.getElementsByClassName("img1")[0].setAttribute("src" , "./images/dice"+randomNum1+".png");
 
-var randomNum2 = Math.ceil((Math.random() * 6));
-document.getElementsByClassName("img2")[0].setAttribute("src" , "./images/dice"+randomNum2+".png");
+    var randomNum2 = Math.ceil((Math.random() * 6));
+    document.getElementsByClassName("img2")[0].setAttribute("src" , "./images/dice"+randomNum2+".png");
 
-if (randomNum1 > randomNum2){
-    document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
-}else if (randomNum2 > randomNum1) {
-    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
-}else{
-    document.querySelector("h1").textContent = "Draw!";
+    if (randomNum1 > randomNum2){
+        document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+    }else if (randomNum2 > randomNum1) {
+        document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+    }else{
+        document.querySelector("h1").textContent = "Draw!";
 }
+}
+)
+
