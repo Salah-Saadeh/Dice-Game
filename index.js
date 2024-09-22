efa5ac355cc223572068993076c5bfa0752e0f03
Salah-Saadeh <119014088+Salah-Saadeh@users.noreply.github.com>
2024-09-22 23:13:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ document.querySelector(".shuffleButton").addEventListener("click", function(){
     document.getElementsByClassName("img2")[0].setAttribute("src" , "./images/dice"+randomNum2+".png");
 
     if (randomNum1 > randomNum2){
-        document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+        document.querySelector("h1").textContent = "💪 Player 1 Wins!";
     }else if (randomNum2 > randomNum1) {
-        document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+        document.querySelector("h1").textContent = "Player 2 Wins! 💪";
     }else{
         document.querySelector("h1").textContent = "Draw!";
 }
